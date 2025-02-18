40dbd11273a149e0195636369ed3282c6f85f1fd
senya2007-oos <korolkovarsenij14@gmail.com>
2025-02-18 14:20:28 +0000

    Update script.js
    Signed-off-by: senya2007-oos <korolkovarsenij14@gmail.com>

--- script.js
+++ script.js
@@ -66,9 +66,5 @@ document.getElementById("pay-with-card").addEventListener("click", async functio
         console.error("Ошибка при запросе на сервер:", err);
     }
 });
-const products = [
-  { id: 1, name: "Товар 1", price: 100 },
-  { id: 2, name: "Товар 2", price: 200 },
-  { id: 3, name: "Товар 3", price: 300 }
-];
+
 
